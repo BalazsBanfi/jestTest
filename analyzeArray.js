bc620ba120b9bdff7610d57afd9cb0296594db77
bally4h <banfibalazs81@gmail.com>
2023-11-05 09:27:24 +0100

    Finish analyzeArray test

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,14 +1,18 @@
 export default function analyzeArray(arr) {
-
-    const message = 'This is not an array or there are some NaN element';
-    if (arr.filter(x => x !== Number.isInteger(x)) > 0 || !arr) {
-        return message
-    }
+  const message =
+    'This is not a real array or there are in some not a number element';
+  if (!Array.isArray(arr) || arr.length === 0 || (arr.filter((x) => !Number.isInteger(x)) || []).length > 0) {
+    return message;
+  } else {
     const length = arr.length;
-    const average = arr.reduce((x, y) => x + y, 0) / length
+    const average = arr.reduce((x, y) => x + y, 0) / length;
     const min = Math.min(...arr);
-    const max = Math.max(...arr); 
+    const max = Math.max(...arr);
     return {
-        average, length, max, min
-    }
+      average,
+      length,
+      max,
+      min,
+    };
+  }
 }
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,10 +1,59 @@
 import analyzeArray from "./analyzeArray";
 
-test('Array of numbers', () => {
-    expect(analyzeArray([1, 2, 3, 4, 5, 6, 7])).toEqual({
-      average: 4,
-      min: 1,
-      max: 7,
-      length: 7,
-    });
+test("Array of numbers [1, 2, 3, 4, 5, 6, 7]", () => {
+  expect(analyzeArray([1, 2, 3, 4, 5, 6, 7])).toEqual({
+    average: 4,
+    min: 1,
+    max: 7,
+    length: 7,
   });
+});
+
+test("Array of bigger and negativ numbers", () => {
+  expect(analyzeArray([-617, 0, 35441, 668911, -6442367, 7])).toEqual({
+    average: -956437.5,
+    min: -6442367,
+    max: 668911,
+    length: 6,
+  });
+});
+
+test("Array with same numbers", () => {
+  expect(analyzeArray([-200, -100, -200, 55, 55, 55, 48])).toEqual({
+    average: -41,
+    min: -200,
+    max: 55,
+    length: 7,
+  });
+});
+
+test("Array with some not numbers", () => {
+  expect(analyzeArray(["aaa", -100, "-200", 55, "55", 55, 53])).toEqual(
+    "This is not a real array or there are in some not a number element"
+  );
+});
+
+test("Empty array", () => {
+  expect(analyzeArray([])).toEqual(
+    "This is not a real array or there are in some not a number element"
+  );
+});
+
+test("Not an array", () => {
+  expect(analyzeArray('FunFun')).toEqual(
+    "This is not a real array or there are in some not a number element"
+  );
+});
+
+test("Not an array", () => {
+  expect(analyzeArray(1234)).toEqual(
+    "This is not a real array or there are in some not a number element"
+  );
+});
+
+
+test("Not an array", () => {
+  expect(analyzeArray({1: 'aaa', 2: 'bbb'})).toEqual(
+    "This is not a real array or there are in some not a number element"
+  );
+});
